037421bf82f031d2cd363af499dcc51daa45f950
Andy <andy.l.hunt85@gmail.com>
2022-10-10 10:58:31 -0400

    Read and updated lesson code halfway through lesson 1-C

--- part1/lesson-practice/src/App.js
+++ part1/lesson-practice/src/App.js
@@ -1,29 +1,15 @@
-const Hello = (props) => {
-  return (
-    <>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
-    </>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Alexi'
-  const age = 37
+  const [ counter, setCounter ] = useState(0)
+  setTimeout(
+    () => setCounter(counter + 1), 1000
+  )
+
+  console.log('rendering...', counter)
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Andy" age={age} />
-      <Hello name={name} age={32} />
-      <Footer />
-    </>
+    <div>{counter}</div>
   )
 }
 
