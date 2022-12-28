a174b969fa7e56274407dc7703c519f237def61a
Andy <andy.l.hunt85@gmail.com>
2022-12-28 15:32:21 -0500

    style(assets): followed lesson to end of 1-C
    Typed and studied code along with lesson 1-C: 'Component state,
    event handlers'

--- part1/lesson-practice/src/App.js
+++ part1/lesson-practice/src/App.js
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  setTimeout(
-    () => setCounter(counter + 1), 1000
-  )
+const Display = ({counter}) => <div>{counter}</div>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-  console.log('rendering...', counter)
+const App = () => {
+  const [ counter, setCounter] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
   )
 }
 
